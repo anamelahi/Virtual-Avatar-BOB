6295afcc862b7e0d6c489317b59561a844a9f122
addy2354 <93484102+addy2354@users.noreply.github.com>
2022-10-28 22:25:13 +0530

    Update pilot.py

--- pilot.py
+++ pilot.py
@@ -1,23 +1,23 @@
-import python_avatars as pa
+import python_avatars as pa #importing main raw materials of the project
 
 my_avatar =  pa.Avatar(
-    style = pa.AvatarStyle.CIRCLE,
-    background_color= pa.BackgroundColor.WHITE,
-    top= pa.HairType.CAESAR_SIDE_PART,
-    eyebrows= pa.EyebrowType.UP_DOWN,
-    eyes= pa.EyeType.EYE_ROLL,
-    nose = pa.NoseType.DEFAULT,
-    mouth= pa.MouthType.EATING,
-    facial_hair= pa.FacialHairType.NONE,
+    style = pa.AvatarStyle.CIRCLE, #Shape = Circular
+    background_color= pa.BackgroundColor.WHITE, #BG=White 
+    top= pa.HairType.CAESAR_SIDE_PART, #Hair
+    eyebrows= pa.EyebrowType.UP_DOWN,   #Eyebrows
+    eyes= pa.EyeType.EYE_ROLL,  #Eyes
+    nose = pa.NoseType.DEFAULT, #Nose
+    mouth= pa.MouthType.EATING,    #Mouth
+    facial_hair= pa.FacialHairType.NONE,    #Beard
 
     #I can use hex colors in any color attribute...
 
-    skin_color= "E8D3BB",
+    skin_color= "E8D3BB", #Don't need to use "#"
 
-    hair_color= pa.HairColor.BLACK,
-    accessory= pa.AccessoryType.NONE,
-    clothing= pa.ClothingType.HOODIE,
-    clothing_color= pa.ClothingColor.HEATHER
+    hair_color= pa.HairColor.BLACK, #colour of hair
+    accessory= pa.AccessoryType.NONE, #Accessories if any 
+    clothing= pa.ClothingType.HOODIE,   #Attires    
+    clothing_color= pa.ClothingColor.HEATHER    #Colour of attires
 )
 
 #Save to a file
